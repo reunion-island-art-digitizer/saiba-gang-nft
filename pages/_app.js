c80f44cf390b04fd1c6d83a46727b63b04efa68f
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-10-04 22:53:28 -0500

    fix: gtag

--- pages/_app.js
+++ pages/_app.js
@@ -19,12 +19,12 @@ const App = ({ Component, pageProps }) => {
       </Head>
       <DefaultSeo {...seo} />
       <Script
-        strategy="lazyLoad"
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
-        strategy="lazyLoad"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
