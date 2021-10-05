316318d3e08b6cccd790d334a859cce8c5c47a13
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-10-05 03:24:38 -0500

    refactor: collapse styles

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -5,8 +5,11 @@ import styles from "styles/Collapse.module.scss";
 export default function Collapse({ title, children }) {
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
+  const classes = `${styles["collapse-section"]}
+    ${isExpanded ? styles["collapse-section--expanded"] : ""}
+  }`;
   return (
-    <div className={styles["collapse-section"]}>
+    <div className={classes}>
       <button {...getToggleProps()} className={styles["collapse-button"]}>
         {isExpanded ? (
           <>
@@ -20,7 +23,11 @@ export default function Collapse({ title, children }) {
           </>
         )}
       </button>
-      <section {...getCollapseProps()}>
+      <section
+        {...getCollapseProps({
+          style: { backgroundColor: "#a851b2" },
+        })}
+      >
         <div className={styles["collapse-content"]}>{children}</div>
       </section>
     </div>
