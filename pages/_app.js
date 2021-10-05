f1993a3328a8fc4a7bf362c9c7055590e38ded06
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-10-05 00:39:36 -0500

    refactor: redeploy to sync vercel env variable

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import { ParallaxProvider } from "react-scroll-parallax";
 import siteConfig from "configs/site-config";
 import "../styles/globals.scss";
 import FontFace from "components/FontFace";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 
 const App = ({ Component, pageProps }) => {
   const { seo } = siteConfig;
@@ -16,6 +18,24 @@ const App = ({ Component, pageProps }) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
       </Head>
       <DefaultSeo {...seo} />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <ParallaxProvider>
         <Component {...pageProps} />
       </ParallaxProvider>
