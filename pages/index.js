f4a8b05d7ba98664bb6bb348ff67efd7e9aa09a0
Quentin Goujon <goujonquentin@hotmail.com>
2021-10-17 01:01:46 +0400

    own and mint

--- pages/index.js
+++ pages/index.js
@@ -106,6 +106,14 @@ export default function Home() {
                     <p>Owning a #RIA Boys allows you to regularly get benefits when we launch future artist #NFTs, RIAD Token, RIAD Marketplace, IDOs and more.</p>
                     <p>This system, which is called Own to Earn, is set up to create a real community around artists.</p>
                     <p>For this, an entire ecosystem will be created around this community through the RIAD project (Reunion Island Artist Digitizer) of which RIA Boys is the first stone.</p>
+                    <Image
+                        src="/images/earn.png"
+                        priority={true}
+                        alt="Own To Earn"
+                        layout="responsive"
+                        width={800}
+                        height={600}
+                    />
        </Section>
     </Container>
                  
