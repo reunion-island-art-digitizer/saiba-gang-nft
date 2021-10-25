985d879a0052d623447083d5241fb13670364dab
Quentin Goujon <goujonquentin@hotmail.com>
2021-10-25 23:30:32 +0400

    faq collapse

---
+++ src/components/Collapse.js
@@ -0,0 +1,35 @@
+import { CaretDown, CaretUp } from "./Icons";
+import useCollapse from "react-collapsed";
+import styles from "styles/Collapse.module.scss";
+
+export default function Collapse({ title, children }) {
+    const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+
+    const classes = `${styles["collapse-section"]}
+    ${isExpanded ? styles["collapse-section--expanded"] : ""}
+  }`;
+    return (
+        <div className={classes}>
+            <button {...getToggleProps()} className={styles["collapse-button"]}>
+                {isExpanded ? (
+                    <>
+                        <CaretUp className="collapse-icon" />
+                        {title}
+                    </>
+                ) : (
+                    <>
+                        <CaretDown className="collapse-icon" />
+                        {title}
+                    </>
+                )}
+            </button>
+            <section
+                {...getCollapseProps({
+                    style: { backgroundColor: "#a851b2" },
+                })}
+            >
+                <div className={styles["collapse-content"]}>{children}</div>
+            </section>
+        </div>
+    );
+}
