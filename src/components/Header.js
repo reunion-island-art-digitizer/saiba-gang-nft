ba97c74e5d296b895e9e63bc58957cd2c4cf5900
Quentin Goujon <goujonquentin@hotmail.com>
2021-11-16 22:29:24 +0400

    trad

--- src/components/Header.js
+++ src/components/Header.js
@@ -323,7 +323,9 @@ export default function Header({ size }) {
             <Instagram className={styles["header-link-social"]} />
           </a>
         </div>
-              <div id="translator_659"><noscript>Javascript is disable - <a href="http://www.supportduweb.com/">http://www.supportduweb.com/</a> - <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction</a></noscript></div><script type="text/javascript" src="http://services.supportduweb.com/translator/659-4-yyyyyyyyy.js"></script>
+              <div>
+                  <div id="translator_659"><noscript>Javascript is disable - <a href="http://www.supportduweb.com/">http://www.supportduweb.com/</a> - <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction</a></noscript></div><script type="text/javascript" src="http://services.supportduweb.com/translator/659-4-yyyyyyyyy.js"></script>
+                  </div>
       </div>
     </header>
   );
