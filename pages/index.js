2e9bab25bd03c02bd0916a2636eb1ddcbb824605
Quentin Goujon <goujonquentin@hotmail.com>
2021-11-16 22:47:55 +0400

    cont trad

--- pages/index.js
+++ pages/index.js
@@ -15,10 +15,7 @@ export default function Home() {
     return (
       <Layout >
             <Banner />
-            <Traduction />
-            <Container>
-            <div id="translator_659"><noscript>Javascript is disable - <a href="http://www.supportduweb.com/">http://www.supportduweb.com/</a> - <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction</a></noscript></div><script type="text/javascript" src="http://services.supportduweb.com/translator/659-4-yyyyyyyyy.js"></script>
-            </Container>
+            <Traduction />            
       <Section id="RIA Boys">
         <Container className="mb-2">
           <h2>RIA Geck</h2>
--- src/components/Traduction.js
+++ src/components/Traduction.js
@@ -1,13 +1,16 @@
 export default function Traduction() {
     return (
-<div id="translator_659">
-            <noscript>Javascript is disable
-            <a href="http://www.supportduweb.com/">http://www.supportduweb.com/
-                </a>
-            <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction
-                </a>
-            </noscript>
-            </div>
-        <script type="text/javascript" src="http://services.supportduweb.com/translator/659-4-yyyyyyyyy.js"></script>
+<Container>
+<div>
+    <noscript>Javascript is disable
+            <a href="http://www.supportduweb.com/"> http://www.supportduweb.com/
+            </a>
+            <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html"> Générateur de boutons de traduction
+            </a>
+    </noscript>
+</div>
+        <script type="text/javascript" src="http://services.supportduweb.com/translator/659-4-yyyyyyyyy.js">
+            </script>
+</Container>
     );
 }
