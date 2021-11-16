db3e22ee0f8050f160677aa39b22136961c13218
Quentin Goujon <goujonquentin@hotmail.com>
2021-11-16 23:00:05 +0400

    trad direct index

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,18 @@ export default function Home() {
     return (
       <Layout >
             <Banner />
-            <Traduction />            
+            <div id="google_translate_element"></div>
+
+            <script>
+                function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({
+                        //here is where you change the language
+                        pageLanguage: 'en'
+                    }, 'google_translate_element');
+}
+            </script>
+
+            <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
       <Section id="RIA Boys">
         <Container className="mb-2">
           <h2>RIA Geck</h2>
--- src/components/Traduction.js
+++ src/components/Traduction.js
@@ -1,14 +1,16 @@
 export default function Traduction() {
     return (
-        <Container>
-            <div id="google_translate_element"></div>
-            <script type="text/javascript"
-                src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-            <cript type="text/javascript">
-                function googleTranslateElementInit() {
-                    new google.translate.TranslateElement({pageLanguage: 'en'},
-                }
-                </script>
-</Container>
+           <div id="google_translate_element"></div>
+
+<script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    //here is where you change the language
+    pageLanguage: 'en'
+  }, 'google_translate_element');
+}
+</script>
+
+<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     );
 }
