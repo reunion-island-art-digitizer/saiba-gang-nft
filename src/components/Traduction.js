68dc7fdf3a8b0085383a781b4e9fa337e9fe32d1
Quentin Goujon <goujonquentin@hotmail.com>
2021-11-16 23:02:45 +0400

    t

--- src/components/Traduction.js
+++ src/components/Traduction.js
@@ -1,16 +1,5 @@
 export default function Traduction() {
     return (
-           <div id="google_translate_element"></div>
-
-<script>
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    //here is where you change the language
-    pageLanguage: 'en'
-  }, 'google_translate_element');
-}
-</script>
-
-<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+          <div id="translator_21982"><noscript>Javascript est desactiv&eacute; - <a href="http://www.supportduweb.com/">Support du web</a> - <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Boutons Traduction enligne site web</a></noscript></div><script type="text/javascript" src="http://services.supportduweb.com/translator/21982-1-yyyyyyyyy.js"></script>
     );
 }
